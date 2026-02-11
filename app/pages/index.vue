b6c095be307c630a80e3290f1a3cfd445f555346
Jamie Evans <taichi.chikung@gmail.com>
2026-02-11 17:31:25 +0000

    feat: add initial Prisma schema with enums and generated client files

---
+++ app/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <UPageHero
+    title="Whisper Wall"
+    headline="100% anonymous, 100% supportive."
+    description="A safe, anonymous space to share what’s on your mind — and get real support from people who care. No identities. No judgment. Just connection."
+    :links="links"
+  />
+
+  <UPageSection title="Categories" description="Explore the different categories of thoughts and responses on Whisper Wall. Whether you're looking to share your own experiences or support others, there's a category for everyone.">
+    <template #features>
+      <UPageCard v-for="c in categories" :key="c" :title="c" icon="i-lucide-hand-helping" class="capitalize" />
+    </template>
+  </UPageSection>
+
+  <UPageSection title="Responses" description="Explore the different categories of responses on Whisper Wall. Whether you're looking to share your own experiences or support others, there's a category for everyone.">
+    <template #features>
+      <UPageCard v-for="c in responseCategories" :key="c" :title="c" icon="i-heroicons-hand-raised-solid" class="capitalize" />
+    </template>
+  </UPageSection>
+</template>
+<script lang="ts" setup>
+import type { ButtonProps } from "@nuxt/ui";
+import { Category, ResponseCategory } from "~~/prisma/generated/client/enums";
+
+const links = ref<ButtonProps[]>([
+  {
+    label: "Share a thought",
+    to: "#",
+    variant: "solid",
+    size: "xl",
+  },
+  {
+    label: "Support someone",
+    to: "#",
+    variant: "outline",
+    size: "xl",
+  },
+]);
+
+const categories = Object.values(Category);
+const responseCategories = Object.values(ResponseCategory);
+</script>
--- prisma/generated/client/internal/class.ts
+++ prisma/generated/client/internal/class.ts
@@ -20,7 +20,7 @@ const config: runtime.GetPrismaClientConfig = {
   "clientVersion": "7.3.0",
   "engineVersion": "9d6ad21cbbceab97458517b147a6a09ff43aa735",
   "activeProvider": "postgresql",
-  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client\"\n  output   = \"../server/prisma-client\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n}\n",
+  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client\"\n  output   = \"./generated/client\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n}\n\nenum Category {\n  stress\n  relationship\n  work\n  family\n  motivation\n  purpose\n  life\n  finance\n  health\n  other\n}\n\nenum ResponseCategory {\n  advice\n  empathy\n  encouragement\n  humor\n  perspective\n  validation\n  support\n  resources\n  motivation\n  other\n}\n",
   "runtimeDataModel": {
     "models": {},
     "enums": {},
