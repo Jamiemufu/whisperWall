dabf988c9d6d1c7d01bd3290298e059c2ff33fcb
Jamie Evans <taichi.chikung@gmail.com>
2026-03-17 11:01:53 +0000

    feat: rearrange response display in WhisperReplies for improved readability

--- app/components/WhisperReplies.vue
+++ app/components/WhisperReplies.vue
@@ -5,13 +5,16 @@
       :key="response.id"
       class="flex flex-col justify-between items-start gap-4 my-2 bg-white p-2 rounded-xl"
     >
-      <div class="flex gap-2 justify-between items-start">
+      <div class="flex gap-2">
         <UIcon
           :name="ResponseCategoryToIcon[response.category]"
           size="sm"
           class="w-5 h-5 text-primary shrink-0 mt-1"
         />
-        <p>{{ response.content }}</p>
+        <p>{{ response.category }}</p>
+      </div>
+      <div>
+        <p class="text-base">{{ response.content }}</p>
       </div>
       <p class="text-sm text-gray-500 italic self-end">
         {{ formatDateShort(response.createdAt) }}
